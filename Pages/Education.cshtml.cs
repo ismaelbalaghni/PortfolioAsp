f2bb3a3c64ecfc89df3c83adc8d761fb6c8d4d4a
Ismaël Balaghni <ismaadu78@outlook.fr>
2023-09-26 19:35:54 +0200

    Add Education pages

---
+++ Pages/Education.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portfolio.Pages
+{
+    public class EducationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
